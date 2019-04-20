2f43260486e77b640341949e2e1f203ead7119df
koonce <erk347@nyu.edu>
2019-04-20 16:26:44 -0400

    Initial commit

---
+++ Assets/DialogueUI.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using TMPro;
+
+public class DialogueUI : Yarn.Unity.DialogueUIBehaviour
+{
+    public GameObject player;
+
+    public GameObject dialogueContainer;
+
+    /// The UI element that displays lines
+    public GameObject lineText;
+
+    /// A UI element that appears after lines have finished appearing
+    public GameObject continuePrompt;
+
+    /// A delegate (ie a function-stored-in-a-variable) that
+    /// we call to tell the dialogue system about what option
+    /// the user selected
+    private Yarn.OptionChooser SetSelectedOption;
+
+    /// How quickly to show the text, in seconds per character
+    [Tooltip("How quickly to show the text, in seconds per character")]
+    public float textSpeed = 0.05f;
+
+    /// The buttons that let the user choose an option
+    public List<Button> optionButtons;
+
+    /// Make it possible to temporarily disable the controls when
+    /// dialogue is active and to restore them when dialogue ends
+    public RectTransform gameControlsContainer;
+
+    void Awake()
+    {
+        // Start by hiding the container, line and option buttons
+        if (dialogueContainer != null)
+            dialogueContainer.SetActive(false);
+
+        //lineText.gameObject.SetActive(false);
+
+        foreach (var button in optionButtons)
+        {
+            button.gameObject.SetActive(false);
+        }
+
+        // Hide the continue prompt if it exists
+        if (continuePrompt != null)
+            continuePrompt.SetActive(false);
+    }
+
+    void Update()
+    {
+        //lineText = GameObject.Find("Dialogue");
+    }
+
+    /// Show a line of dialogue, gradually
+    public override IEnumerator RunLine(Yarn.Line line)
+    {
+
+        TextMeshPro tmp = lineText.GetComponent<TextMeshPro>();
+        // Show the text
+        lineText.gameObject.SetActive(true);
+
+        if (textSpeed > 0.0f)
+        {
+            // Display the line one character at a time
+            var stringBuilder = new StringBuilder();
+
+            foreach (char c in line.text)
+            {
+                stringBuilder.Append(c);
+
+                tmp.text = stringBuilder.ToString();
+                yield return new WaitForSeconds(textSpeed);
+            }
+        }
+        else
+        {
+            // Display the line immediately if textSpeed == 0
+            tmp.text = line.text;
+        }
+
+        // Show the 'press any key' prompt when done, if we have one
+        if (continuePrompt != null)
+            continuePrompt.SetActive(true);
+
+        // Wait for any user input
+        while (Input.GetKeyDown(KeyCode.Space) == false)
+        {
+            yield return null;
+        }
+
+        // Hide the text and prompt
+        lineText.gameObject.SetActive(false);
+
+        if (continuePrompt != null)
+            continuePrompt.SetActive(false);
+
+    }
+
+    /// Show a list of options, and wait for the player to make a selection.
+    public override IEnumerator RunOptions(Yarn.Options optionsCollection,
+                                            Yarn.OptionChooser optionChooser)
+    {
+        // Do a little bit of safety checking
+        if (optionsCollection.options.Count > optionButtons.Count)
+        {
+            Debug.LogWarning("There are more options to present than there are" +
+                             "buttons to present them in. This will cause problems.");
+        }
+
+        // Display each option in a button, and make it visible
+        int i = 0;
+        foreach (var optionString in optionsCollection.options)
+        {
+            optionButtons[i].gameObject.SetActive(true);
+            optionButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = optionString;
+            i++;
+        }
+
+        // Record that we're using it
+        SetSelectedOption = optionChooser;
+
+        // Wait until the chooser has been used and then removed (see SetOption below)
+        while (SetSelectedOption != null)
+        {
+            yield return null;
+        }
+
+        // Hide all the buttons
+        foreach (var button in optionButtons)
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+
+    /// Called by buttons to make a selection.
+    public void SetOption(int selectedOption)
+    {
+
+        // Call the delegate to tell the dialogue system that we've
+        // selected an option.
+        SetSelectedOption(selectedOption);
+
+        // Now remove the delegate so that the loop in RunOptions will exit
+        SetSelectedOption = null;
+    }
+
+    /// Run an internal command.
+    public override IEnumerator RunCommand(Yarn.Command command)
+    {
+        // "Perform" the command
+        Debug.Log("Command: " + command.text);
+
+        // if (command.text == "jump")
+        // {
+        //     player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 300));
+        // }
+
+
+
+        yield break;
+    }
+
+    /// Called when the dialogue system has started running.
+    public override IEnumerator DialogueStarted()
+    {
+        Debug.Log("Dialogue starting!");
+
+        // Enable the dialogue controls.
+        if (dialogueContainer != null)
+            dialogueContainer.SetActive(true);
+
+        // Hide the game controls.
+        if (gameControlsContainer != null)
+        {
+            gameControlsContainer.gameObject.SetActive(false);
+        }
+
+        yield break;
+    }
+
+    /// Called when the dialogue system has finished running.
+    public override IEnumerator DialogueComplete()
+    {
+        Debug.Log("Complete!");
+
+        // Hide the dialogue interface.
+        if (dialogueContainer != null)
+            dialogueContainer.SetActive(false);
+
+        // Show the game controls.
+        if (gameControlsContainer != null)
+        {
+            gameControlsContainer.gameObject.SetActive(true);
+        }
+
+        yield break;
+    }
+
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Movement : MonoBehaviour
+{
+    public float step = 1;
+    public GameObject dr;
+    bool dialogueStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DialogueRunner drScript = dr.GetComponent<DialogueRunner>();
+        Vector3 pos = this.transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= step;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += step;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= step;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += step;
+        }
+
+        this.transform.position = pos;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("NPC"))
+        {
+            if (!dialogueStarted)
+            {
+                startDialog();
+                dialogueStarted = true;
+            }
+        }
+    }
+
+    
+    void startDialog()
+    {
+        FindObjectOfType<DialogueRunner>().StartDialogue();
+    }
+
+}
