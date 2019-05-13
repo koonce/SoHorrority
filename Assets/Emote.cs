f5689192c56fcaf378fabdb39bfca868554ff056
koonce <erk347@nyu.edu>
2019-05-13 02:12:15 -0400

    entire first scene is working!
    yayayaya
    so dialogue works,
    text box moves,
    still looks bad but
    COOL

---
+++ Assets/Emote.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity
+{
+    public class Emote : MonoBehaviour
+    {
+        public Sprite sad, happy, normal;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        [YarnCommand("Sad")]
+        public void SadFace()
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = sad;
+        }
+
+        [YarnCommand("Happy")]
+        public void HappyFace()
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = happy;
+        }
+
+        [YarnCommand("Normal")]
+        public void NormalFace()
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = normal;
+        }
+    }
+}
