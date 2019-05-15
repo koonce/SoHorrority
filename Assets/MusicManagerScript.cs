dd39dd56f05d9bc420e0e25b1669343cd0268063
corduroyqueen <aa5056@nyu.edu>
2019-05-15 13:00:48 -0400

    frinight music is happening

--- Assets/MusicManagerScript.cs
+++ Assets/MusicManagerScript.cs
@@ -10,8 +10,10 @@ public class MusicManagerScript : MonoBehaviour
     AudioSource ouijaMusic;
     public GameObject speech;
     public AudioClip song;
+    public AudioClip song2;
     bool ouijaBool = false;
-    int volumeBool = 0;
+    public float volumeBool = 0;
+    GameObject musicPlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,18 +27,28 @@ public class MusicManagerScript : MonoBehaviour
         {
             if (ouijaMusic == null)
             {
-                ouijaMusic = gameObject.AddComponent<AudioSource>();
+                musicPlayer = new GameObject("ok");
+                ouijaMusic = musicPlayer.gameObject.AddComponent<AudioSource>();
                 ouijaMusic.clip = song;
-                
             }
-            if(speech.GetComponent<TextMeshPro>().text== "I will summon your sister.")
+            if(speech.GetComponent<TextMeshPro>().text== "I will summon your sister." && !ouijaBool)
             {
                 ouijaBool = true;
+                ouijaMusic.Play();
             }
         }
         if (ouijaBool)
         {
-
+            if (volumeBool < 1) { volumeBool += 0.0001f; }
+            mainAudioSource.volume = 1 - volumeBool;
+            ouijaMusic.volume = volumeBool + 0.5f;
+            
+            if (ouijaMusic.isPlaying == false)
+            {
+                ouijaMusic.clip = song2;
+                ouijaMusic.Play();
+                ouijaMusic.loop = true;
+            }
         }
     }
 }
