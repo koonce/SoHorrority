5358c2c4dd4dcd76c5a8f29cb7cbce66307ee5dd
corduroyqueen <aa5056@nyu.edu>
2019-05-15 12:45:06 -0400

    dont touch the music manager script

--- Assets/MusicManagerScript.cs
+++ Assets/MusicManagerScript.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 public class MusicManagerScript : MonoBehaviour
 {
+    AudioSource mainAudioSource;
     AudioSource ouijaMusic;
+    public GameObject speech;
+    public AudioClip song;
+    bool ouijaBool = false;
+    int volumeBool = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,7 +26,17 @@ public class MusicManagerScript : MonoBehaviour
             if (ouijaMusic == null)
             {
                 ouijaMusic = gameObject.AddComponent<AudioSource>();
+                ouijaMusic.clip = song;
+                
             }
+            if(speech.GetComponent<TextMeshPro>().text== "I will summon your sister.")
+            {
+                ouijaBool = true;
+            }
+        }
+        if (ouijaBool)
+        {
+
         }
     }
 }
