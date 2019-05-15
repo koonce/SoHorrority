6519200e90f7d0e9836c47ea33d688f2f0c21f64
corduroyqueen <aa5056@nyu.edu>
2019-05-15 12:09:37 -0400

    fixed a couple small things

--- Assets/MusicManagerScript.cs
+++ Assets/MusicManagerScript.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MusicManagerScript : MonoBehaviour
 {
+    AudioSource ouijaMusic;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,12 @@ public class MusicManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(SceneManager.GetActiveScene().name == "Frinight")
+        {
+            if (ouijaMusic == null)
+            {
+                ouijaMusic = gameObject.AddComponent<AudioSource>();
+            }
+        }
     }
 }
