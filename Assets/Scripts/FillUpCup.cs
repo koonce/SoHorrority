1f5b63c0149826fba64853ac255de3f76c27d769
koonce <erk347@nyu.edu>
2019-05-15 14:07:23 -0400

    saturnight works

--- Assets/Scripts/FillUpCup.cs
+++ Assets/Scripts/FillUpCup.cs
@@ -1,58 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
+using UnityEngine.SceneManagement;
 
-public class FillUpCup : MonoBehaviour
-{
-
-    bool cupFull = false;
-
-    public Sprite fillCup, emptyCup, bloodCup;
-
-    public int drinks = 0;
-
-    public GameObject background;
-    public Sprite dizzyBackground;
-
-    float speed = 1.0f; //how fast it shakes
-    float amount = 1.0f; //how much it shakes
-
-    // Start is called before the first frame update
-    void Start()
+    public class FillUpCup : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!cupFull)
+        bool cupFull = false;
+
+        public Sprite fillCup, emptyCup, bloodCup;
+
+        public int drinks = 0;
+
+        public GameObject background;
+        public Sprite dizzyBackground;
+
+        public GameObject dr;
+        DialogueRunner drScript;
+
+        public string node;
+
+        float speed = 1.0f; //how fast it shakes
+        float amount = 1.0f; //how much it shakes
+
+        public bool start = true;
+
+        public int current = 0;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            if (transform.position.y < -11)
-            {
-                if (drinks > 5)
-                {
-                    GetComponent<SpriteRenderer>().sprite = bloodCup;
-                    background.GetComponent<SpriteRenderer>().sprite = dizzyBackground;
-                }
-                else
-                {
-                    GetComponent<SpriteRenderer>().sprite = fillCup;
-                }
-                cupFull = true;
-            }
+            drScript = dr.GetComponent<DialogueRunner>();
         }
 
-        if (cupFull)
+        // Update is called once per frame
+        void Update()
         {
-            if (transform.position.y > 8)
+            if (!cupFull && !start)
             {
-                GetComponent<SpriteRenderer>().sprite = emptyCup;
-                drinks++;
-                cupFull = false;
+                if (transform.position.y < -11)
+                {
+                    if (drinks > 3 && current == 1)
+                    {
+                        node = "BloodShotsFirst";
+                        drScript.startNode = node;
+                        startDialog();
+                        start = true;
+                    }
+                    else if (drinks > 7 && current == 2)
+                    {
+                        node = "BloodShotsSecond";
+                        drScript.startNode = node;
+                        startDialog();
+                        GetComponent<SpriteRenderer>().sprite = bloodCup;
+                        background.GetComponent<SpriteRenderer>().sprite = dizzyBackground;
+                        start = true;
+
+                }
+                    else if (drinks > 11 && current == 3)
+                    {
+                        node = "BloodShotsThird";
+                        drScript.startNode = node;
+                        startDialog();
+                        start = true;
+                    }
+                    else
+                    {
+                        GetComponent<SpriteRenderer>().sprite = fillCup;
+                    }
+                    cupFull = true;
+                }
             }
+
+            if (cupFull)
+            {
+                if (transform.position.y > 8)
+                {
+                    GetComponent<SpriteRenderer>().sprite = emptyCup;
+                    drinks++;
+                    cupFull = false;
+                }
+            }
+
+
         }
 
-
+        void startDialog()
+        {
+            FindObjectOfType<DialogueRunner>().StartDialogue();
+        }
     }
-}
+
---
+++ Assets/UpdateCurrentDialogue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Yarn.Unity
+{
+    public class UpdateCurrentDialogue : MonoBehaviour
+    {
+
+        FillUpCup fuc;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        [YarnCommand("UpdateCurrent")]
+        public void ChangeCurrent()
+        {
+            this.GetComponent<FillUpCup>().current++;
+            this.GetComponent<FillUpCup>().start = false;
+        }
+
+
+        [YarnCommand("NextScene")]
+        public void ChangeScene()
+        {
+            SceneManager.LoadScene("Sunday");
+        }
+    }
+}
